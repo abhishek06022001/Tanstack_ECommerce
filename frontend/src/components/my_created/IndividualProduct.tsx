0eb7aa3f980b165a3693d90d7322b36e0cefb9b6
Abhishek <satheabhishek1@gmail.com>
2024-12-18 09:36:50 +0530

    updation left

--- frontend/src/components/my_created/IndividualProduct.tsx
+++ frontend/src/components/my_created/IndividualProduct.tsx
@@ -1,6 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { Button } from '../ui/button';
 "use client"
@@ -18,34 +18,45 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 const formSchema = z.object({
-    name: z.string().min(2).max(50),
+    name: z.string().min(2).max(100),
     description: z
         .string()
         .min(10, {
             message: "Bio must be at least 10 characters.",
-        })
-        .max(160, {
-            message: "Bio must not be longer than 30 characters.",
         }),
+    price: z.coerce.number().min(1, { message: "The price value must be greater than 0" }),
+    category: z.string()
 })
 type Props = {}
+
 function IndividualProduct({ }: Props) {
     const { id } = useParams();
+    const ac_token = localStorage.getItem('accessToken');
+    const updateProduct = async (product: any) => {
+        return axios.put(`/api/update_product/${id}`, product, {
+            headers: {
+                token: ac_token
+            }
+        });
+    }
     const navigate = useNavigate();
     const role = localStorage.getItem('user_role');
-
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(values)
-    }
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             name: '',
-            description: ''
+            description: ' ',
+            price: 0,
+            category: "none"
         },
     })
-    const ac_token = localStorage.getItem('accessToken');
+    const { mutate } = useMutation({
+        mutationFn: updateProduct
+    });
+
+    // const ac_token = localStorage.getItem('accessToken');
     const { data, isLoading, isError } = useQuery({
         queryKey: ['products', id],
         queryFn: () => {
@@ -56,38 +67,40 @@ function IndividualProduct({ }: Props) {
             });
         }
     })
+
+    // CALLING OF ALL HOOKS MUST BE SAME ACROSS ALL THE CYCLES AND NOT CONDITIONAL DUDE 
+    useEffect(() => {
+        if (data?.data.msg) {
+            form.reset({
+                name: data?.data.msg.name,
+                description: data?.data.msg.description,
+                price: data?.data.msg.price,
+                category: data?.data.msg.category,
+            })
+        }
+    }, [data]);
     if (isLoading) {
         return <div>Loading...</div>
     }
     if (isError) {
-        return <div>Error found...</div>
+        return <div>Error Found...</div>
+    }
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+        mutate(values);
     }
     const product_info = data?.data.msg;
-
-    console.log("The product info is", product_info);
     const moveBack = () => {
         navigate(-1);
     }
-    // useEffect(() => {
-
-    //     if (data?.data.msg) {
-    //         form.reset({
-    //             name: data?.data.msg.name
-    //         })
-    //     }
-
-    // }, []);
-
     return (
         <div className='flex justify-center items-center h-full' >
-
             <div>
-                <div>   <Button onClick={moveBack}>Back</Button></div>
+                <div> <Button className='m-5' onClick={moveBack}>Back</Button></div>
                 <div className='flex gap-3' >
-
                     <div
-                        className="bg-white rounded-sm p-4 flex justify-center
-                            hover:shadow-pink-500 hover:shadow-lg" >
+                        className="bg-white rounded-sm p-4 flex justify-center hover:shadow-pink-500 hover:shadow-lg" >
                         <img className={`md:h-96 h-24`} src={"http://localhost:8080/" + product_info.image} alt="" />
                     </div>
                     <div>
@@ -103,7 +116,6 @@ function IndividualProduct({ }: Props) {
                                                 <Input  {...field} />
                                             </FormControl>
                                             <FormDescription>
-                                                This is your product Name
                                             </FormDescription>
                                             <FormMessage />
                                         </FormItem>
@@ -117,7 +129,6 @@ function IndividualProduct({ }: Props) {
                                             <FormLabel>Products description here</FormLabel>
                                             <FormControl>
                                                 <Textarea
-
                                                     className="resize-none"
                                                     rows={5} cols={70}
                                                     {...field}
@@ -128,15 +139,60 @@ function IndividualProduct({ }: Props) {
                                         </FormItem>
                                     )}
                                 />
+                                <FormField
+                                    control={form.control}
+                                    name="price"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Products price here</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    className="resize-none"
+                                                    type='number'
+                                                    {...field}
+                                                />
+                                            </FormControl>
+
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="category"
+                                    render={({ field }) => (
+                                        <FormItem >
+                                            <FormLabel >Category</FormLabel>
+                                            <FormControl  >
+                                                <Select
+
+                                                    defaultValue={field.value}
+                                                >
+                                                    <SelectTrigger className="w-[380px] ">
+                                                        <SelectValue
+                                                        />
+                                                    </SelectTrigger>
+                                                    <SelectContent className=""  >
+                                                        <SelectItem value="none">none</SelectItem>
+                                                        <SelectItem value="jewelary">Jewelary</SelectItem>
+                                                        <SelectItem value="mens">Men's Clothing</SelectItem>
+                                                        <SelectItem value="electronics">Electronics</SelectItem>
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormDescription>
+                                                This is your product category.
+                                            </FormDescription>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
                                 <Button type="submit">Submit</Button>
                             </form>
                         </Form>
-
                     </div>
-
                 </div>
             </div>
-
         </div>
     )
 }
